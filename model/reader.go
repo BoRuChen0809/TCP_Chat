5fa53f88f7fd46584c90aba1e0677d89506bdcf0
BoRu Chen <chenboru0124@gmail.com>
2022-02-16 12:36:44 +0800

    共用元件-reader

---
+++ model/reader.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	My_cmd "TCP_Chat/model/command"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+type CommandReader struct {
+	reader *bufio.Reader
+}
+
+func NewCmdReader(r io.Reader) *CommandReader {
+	return &CommandReader{reader: bufio.NewReader(r)}
+}
+
+func (r *CommandReader) Read() (interface{}, error) {
+	cmd, err := r.reader.ReadString(' ')
+	if err != nil {
+		return nil, err
+	}
+
+	switch cmd {
+	case "Send ":
+		msg, err := r.reader.ReadString('\n')
+		return My_cmd.SendMsgCommand{Msg: msg[:len(msg)-1]}, err
+	case "SetName ":
+		name, err := r.reader.ReadString('\n')
+		return My_cmd.SetNameCommand{Name: name[:len(name)-1]}, err
+	case "Broadcast ":
+		name, err := r.reader.ReadString(' ')
+		if err != nil {
+			return nil, err
+		}
+		msg, err := r.reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		return My_cmd.BroadcastCommand{Name: name[:len(name)-1], Msg: msg[:len(msg)-1]}, nil
+	default:
+		return nil, fmt.Errorf("nuknown msg")
+	}
+
+}
---
+++ model/reader_test.go
@@ -0,0 +1,35 @@
+package model_test
+
+import (
+	"TCP_Chat/model"
+	My_cmd "TCP_Chat/model/command"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func Test_Reader(t *testing.T) {
+	type test_case struct {
+		cmd_str string
+		expect  interface{}
+	}
+
+	tt := []test_case{
+		{expect: My_cmd.BroadcastCommand{Name: "BoBo", Msg: "Test Message"},
+			cmd_str: "Broadcast BoBo Test Message\n"},
+		{expect: My_cmd.SetNameCommand{Name: "BoBo"}, cmd_str: "SetName BoBo\n"},
+		{expect: My_cmd.SendMsgCommand{Msg: "Test Message"}, cmd_str: "Send Test Message\n"},
+	}
+
+	for _, tc := range tt {
+		r := model.NewCmdReader(strings.NewReader(tc.cmd_str))
+		cmd, err := r.Read()
+		t.Log(cmd)
+		if err != nil {
+			t.Errorf("read error : %v\n", err)
+		}
+		if !reflect.DeepEqual(cmd, tc.expect) {
+			t.Errorf("output and expect are not same")
+		}
+	}
+}
