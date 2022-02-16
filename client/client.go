ad8e4b67ae57c7967c25b6b7f1bf795360618642
BoRu Chen <chenboru0124@gmail.com>
2022-02-16 12:39:20 +0800

    定義client主要工作

---
+++ client/client.go
@@ -0,0 +1,9 @@
+package client
+
+type My_Client interface {
+	Dial(address string) error //連線
+	SetName(name string) error //設定名字
+	SendMsg(msg string) error  //傳訊息
+	StartReceive()             //接收server廣播
+	Close()                    //關閉連線
+}
