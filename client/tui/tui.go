f39de885b768fada8505fc9b446b184de5555f6b
BoRu Chen <chenboru0124@gmail.com>
2022-02-17 14:43:59 +0800

    完成新介面

--- client/tui/tui.go
+++ client/tui/tui.go
@@ -9,26 +9,48 @@ import (
 )
 
 func start_ui(cli *client.Chat_Client) {
+	//使用者名稱
+	name := tui.NewEntry()
+	name.SetSizePolicy(tui.Expanding, tui.Maximum)
+	name.SetFocused(true)
+	nameBox := tui.NewVBox(tui.NewLabel("Input your nickname : "), name)
+	nameBox.SetBorder(true)
+	nameBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+	name.OnSubmit(func(entry *tui.Entry) {
+		if entry.Text() != "" {
+			cli.SetName(entry.Text())
+		}
+	})
+
+	//切換聊天室
+	room := tui.NewEntry()
+	room.SetSizePolicy(tui.Expanding, tui.Maximum)
+	room.SetFocused(false)
+	roomBox := tui.NewVBox(tui.NewLabel("Input Room ID : "), room)
+	roomBox.SetBorder(true)
+	roomBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+	room.OnSubmit(func(entry *tui.Entry) {
+		if entry.Text() != "" {
+			cli.ChangeRoom(entry.Text())
+		}
+	})
+	//左邊區域
+	sidebar := tui.NewVBox(
+		nameBox,
+		tui.NewLabel(""),
+		roomBox,
+		tui.NewSpacer(),
+	)
+	sidebar.SetBorder(true)
+
 	//對話紀錄
 	history := tui.NewVBox()
 	historyScroll := tui.NewScrollArea(history)
 	historyScroll.SetAutoscrollToBottom(true)
 	historyBox := tui.NewVBox(historyScroll)
 	historyBox.SetBorder(true)
-	//使用者名稱
-	name := tui.NewEntry()
-	name.SetSizePolicy(tui.Expanding, tui.Maximum)
-	name.SetFocused(true)
-	nameBox := tui.NewHBox(tui.NewLabel("Set your name : "), name)
-	nameBox.SetBorder(true)
-	nameBox.SetSizePolicy(tui.Expanding, tui.Maximum)
-	name.OnSubmit(func(entry *tui.Entry) {
-		if entry.Text() != "" {
-			cli.SetName(entry.Text())
-			entry.SetText("")
-		}
-	})
-	//輸入框
+
+	//訊息輸入框
 	input := tui.NewEntry()
 	input.SetFocused(false)
 	input.SetSizePolicy(tui.Expanding, tui.Maximum)
@@ -41,23 +63,32 @@ func start_ui(cli *client.Chat_Client) {
 			entry.SetText("")
 		}
 	})
-
-	chat := tui.NewVBox(historyBox, inputBox, nameBox)
+	//右邊區域
+	chat := tui.NewVBox(historyBox, inputBox)
 	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
 
-	ui, err := tui.New(chat)
+	whole_frame := tui.NewHBox(sidebar, chat)
+
+	ui, err := tui.New(whole_frame)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	ui.SetKeybinding("Esc", func() { ui.Quit() })
 	ui.SetKeybinding("Ctrl+n", func() {
+		room.SetFocused(false)
 		name.SetFocused(true)
 		input.SetFocused(false)
 	})
 	ui.SetKeybinding("Ctrl+t", func() {
+		room.SetFocused(false)
 		name.SetFocused(false)
 		input.SetFocused(true)
+	})
+	ui.SetKeybinding("Ctrl+r", func() {
+		room.SetFocused(true)
+		name.SetFocused(false)
+		input.SetFocused(false)
 	})
 
 	go func() {
